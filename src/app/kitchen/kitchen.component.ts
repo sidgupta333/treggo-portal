8159f2bead7374d41f4b30afeeb7962b961abb35
Siddhant Gupta <siddhantgupta993@gmail.com>
2021-01-06 19:43:15 +0530

    solved ng build issues

--- src/app/kitchen/kitchen.component.ts
+++ src/app/kitchen/kitchen.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { RestService } from 'src/app/services/rest.service';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { RestService } from "src/app/services/rest.service";
+import Swal from "sweetalert2";
+import { Router } from '@angular/router';
+import { UtilsService } from "src/app/services/utils.service";
 
 @Component({
-  selector: 'app-kitchen',
-  templateUrl: './kitchen.component.html',
-  styleUrls: ['./kitchen.component.css']
+  selector: "app-kitchen",
+  templateUrl: "./kitchen.component.html",
+  styleUrls: ["./kitchen.component.css"],
 })
 export class KitchenComponent implements OnInit {
-
   ordersList: any = [];
   searchRecord: any;
   p: any;
   dishesList: any = [];
   quantitiesList: any = [];
 
- constructor(private rest: RestService) { }
-
+  constructor(
+    private rest: RestService,
+    private utils: UtilsService,
+    private router: Router
+  ) {}
 
   loggedInTime: any = new Date().toLocaleTimeString();
 
-
-ngOnInit() {
-
+  ngOnInit() {
     this.updateStatusTable();
 
     window.setInterval(() => {
@@ -30,13 +32,11 @@ ngOnInit() {
     }, 10000);
   }
 
-
   updateStatusTable() {
     this.rest.getDrilldown().subscribe((res: any) => {
       this.ordersList = res;
     });
   }
-
 
   selectOrder(item: any) {
     this.dishesList = item.dishes;
@@ -44,48 +44,55 @@ ngOnInit() {
     this.quantitiesList = item.quantities;
   }
 
-
   prepareOrder(item: any) {
-
     let dto = {
       status: "PREPARING",
-      subOrderId: item.sub_order_id
+      subOrderId: item.sub_order_id,
     };
 
-    this.rest.updateSubOrder(dto).subscribe((res: any) => {
-      this.updateStatusTable();
-    },
-      err => {
+    this.rest.updateSubOrder(dto).subscribe(
+      (res: any) => {
+        this.updateStatusTable();
+      },
+      (err) => {
         Swal.fire({
-          title: 'Error!',
-          text: 'Unable to update status',
-          icon: 'error',
-          confirmButtonText: 'OK'
-
+          title: "Error!",
+          text: "Unable to update status",
+          icon: "error",
+          confirmButtonText: "OK",
         });
-      });
+      }
+    );
   }
-
-
 
   completeOrder(item: any) {
     let dto = {
       status: "COMPLETED",
-      subOrderId: item.sub_order_id
+      subOrderId: item.sub_order_id,
     };
 
-    this.rest.updateSubOrder(dto).subscribe((res: any) => {
-      this.updateStatusTable();
-    },
-      err => {
+    this.rest.updateSubOrder(dto).subscribe(
+      (res: any) => {
+        this.updateStatusTable();
+      },
+      (err) => {
         Swal.fire({
-          title: 'Error!',
-          text: 'Unable to update status',
-          icon: 'error',
-          confirmButtonText: 'OK'
-
+          title: "Error!",
+          text: "Unable to update status",
+          icon: "error",
+          confirmButtonText: "OK",
         });
-      });
+      }
+    );
   }
 
+  // Logout user:
+  logout() {
+    this.utils.setLoggedStatus(false);
+    sessionStorage.removeItem("submenu");
+    sessionStorage.removeItem("username");
+    sessionStorage.removeItem("name");
+    sessionStorage.setItem("tenant", null);
+    this.router.navigate([""]);
+  }
 }
